ed34fb719a7ad32f3285e25732265381ccb4da0a
Simon Oram <simon@electrosoup.co.uk>
2010-12-30 02:12:35 +0000

    added static file server, for the fonts

--- soupfontserver/server.py
+++ soupfontserver/server.py
@@ -1,15 +1,17 @@
 from soupfontserver.static import StaticDir
 from paste.urlparser import StaticURLParser
 from webob import Request
+from webob import Response
+
 class ServerFactory(object):
     
     def __init__(self, global_config):
         self.global_config = global_config
         
 
-    def __call__(self, request, handler):
-        self.request = Request(request)
-        self.static = StaticDir(self.global_config, self.request)
-        self.handler = handler
-        static = self.static()
+    def __call__(self, environ, start_response):
+        self.request = Request(environ)
+        self.static = StaticDir(self.request, self.global_config)
+        res = self.static()
+        return res(environ, start_response)
         
--- soupfontserver/static.py
+++ soupfontserver/static.py
@@ -3,23 +3,24 @@ from ConfigParser import NoSectionError, NoOptionError
 from paste.urlparser import StaticURLParser
 
 class StaticDir(object):
-    def __init__(self, global_config, request):
-        self.global_config = global_config
+
+    def __init__(self, request, global_config):
         self.request = request
+        self.environ = self.request.environ
         config = ConfigParser.ConfigParser()
         config_file = config.readfp(open('%s/setup.cfg' % global_config['here']))
         
         try:
             self.font_directory = config.get('font_directory', 'dir')
-        
+
         except (NoSectionError, NoOptionError):
             print "You need to specify a [font_directory] and dir value in the setup.cfg file."
             raise
-            
+        
+        app = StaticURLParser(self.font_directory)
+        self.app = app
+
     def __call__(self):
-        static = StaticURLParser(self.font_directory)
-        import ipdb; ipdb.set_trace()
-        return static
-        
-        
-        
+        self.request.blank('/')
+        res = self.request.get_response(self.app)
+        return res
